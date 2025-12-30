e52daf1b4e735d262d20f16c657752cb757ef43e
sao90 <s.ostervig@hotmail.com>
2025-12-30 15:18:14 +0100

    feat(data_cleaning.py): add more cleaning methods based on exploration

--- src/data_cleaning/data_cleaning.py
+++ src/data_cleaning/data_cleaning.py
@@ -1,5 +1,6 @@
 import sqlite3
 import logging
+from typing import List
 
 import pandas as pd
 
@@ -7,15 +8,29 @@ logger = logging.getLogger(__name__)
 
 
 class DataCleaner:
-    def __init__(self, db_path):
+    """
+    Class for cleaning data in an SQLite database.
+    Provides methods to clean data by removing unwanted records based on
+    specific criteria.
+
+    To be used as a context manager to ensure connection is opened and closed properly.
+    Example usage:
+    ```
+        with DataCleaner(db_path="path/to/db.sqlite") as cleaner:
+            cleaned_data_df = cleaner.run(
+                source_table="my_table",
+                target_table="cleaned_table",
+                countries=["United Kingdom"]
+            )
+    ```
+    """
+
+    def __init__(self, db_path: str):
         self.db_path = db_path
 
     def __enter__(self):
         """
-        Open database connection when entering context manager.
-
-        Called automatically by 'with' statement. Establishes connection
-        and cursor for use in context block.
+        Establish DB connection and initialize cursor within context.
 
         Returns:
             self: DataCleaner instance with active database connection.
@@ -26,11 +41,11 @@ class DataCleaner:
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         """
-        Close database connection when exiting context manager.
-        Commit changes if no exception, else rollback.
-        Captures exceptions automatically from context block.
+        Secure handling of data manipulations within context.
+        Closes database connection when exiting context manager.
+        Commit changes to DB if no exception, else rollback.
 
-        Args:
+        Args: (captured automatically within context block)
             exc_type: Exception type caught, or None if no exception.
             exc_val: Exception instance caught, or None if no exception.
             exc_tb: Exception traceback object caught, or None if no exception.
@@ -45,6 +60,34 @@ class DataCleaner:
                 exc_info=(exc_type, exc_val, exc_tb),
             )
         self.db_connection.close()
+
+    def run(
+        self, source_table: str, target_table: str, countries: List[str]
+    ) -> pd.DataFrame:
+        """
+        Run the full data cleaning process:
+        1. Copy source_table to target_table
+        2. Remove cancelled transactions
+        3. Remove negative values
+        4. Remove articles with alphanumeric StockCode prefix
+        5. Keep only specified countries
+
+        Args:
+            source_table: Name of the source table.
+            target_table: Name of the target table.
+            countries: List of country names to keep.
+
+        Returns:
+            DataFrame containing the cleaned data.
+        """
+        self.copy_table(source_table, target_table)
+        self.remove_cancelled_transactions(target_table)
+        self.remove_negative_values(target_table)
+        self.remove_articles_with_alphanummeric_prefix(target_table)
+        self.keep_countries(target_table, countries)
+        query = f""" SELECT * FROM {target_table} """
+        df = self.query_to_df(query=query)
+        return df
 
     def copy_table(self, source_table: str, target_table: str) -> str:
         """
@@ -80,6 +123,66 @@ class DataCleaner:
         deleted_count = self.cursor.rowcount
         logger.info(
             f"Removed {deleted_count} cancelled transactions from '{table_name}'"
+        )
+
+    def remove_negative_values(
+        self,
+        table_name: str,
+    ) -> None:
+        """
+        Remove rows with zero and negative values in 'Quantity' and 'UnitPrice' columns.
+        Args:
+            table_name: Name of the table to clean.
+        """
+        self.cursor.execute(
+            f"""
+            DELETE FROM {table_name}
+            WHERE Quantity <= 0 OR UnitPrice <= 0
+            """
+        )
+        deleted_count = self.cursor.rowcount
+        logger.info(
+            f"Removed {deleted_count} rows with zero or negative values in 'Quantity' or 'UnitPrice' from '{table_name}'"
+        )
+
+    def remove_articles_with_alphanummeric_prefix(
+        self,
+        table_name: str,
+    ) -> None:
+        """
+        Remove rows where 'StockCode' starts with letters.
+        Investigation shows these are mainly non-product codes, with a few grey areas.
+        Args:
+            table_name: Name of the table to clean.
+        """
+        self.cursor.execute(
+            f"""
+            DELETE FROM {table_name}
+            WHERE StockCode GLOB '[A-Za-z]*'
+            """
+        )
+        deleted_count = self.cursor.rowcount
+        logger.info(
+            f"Removed {deleted_count} rows with alphanumeric 'StockCode' from '{table_name}'"
+        )
+
+    def keep_countries(self, table_name: str, countries: List[str]) -> None:
+        """
+        Keep only rows where 'Country' is in the specified list of countries.
+        Args:
+            table_name: Name of the table to clean.
+            countries: List of country names to keep.
+        """
+        countries_str = ", ".join(f"'{country}'" for country in countries)
+        self.cursor.execute(
+            f"""
+            DELETE FROM {table_name}
+            WHERE Country NOT IN ({countries_str})
+            """
+        )
+        deleted_count = self.cursor.rowcount
+        logger.info(
+            f"Removed {deleted_count} rows not in specified countries from '{table_name}'"
         )
 
     def query_to_df(self, query: str) -> pd.DataFrame:
